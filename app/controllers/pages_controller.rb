56b8887b7d2012c635df1441e39c0ce68d2eb86d
Hender Cuellar <hender.cuellar02@gmail.com>
2021-10-20 13:04:13 -0400

    Coaching estúpido listo

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def new
+  end
+
+  def score
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/new'
+  get 'pages/score'
   get 'ask', to: "questions#ask"
   get 'answer', to: "questions#answer"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
